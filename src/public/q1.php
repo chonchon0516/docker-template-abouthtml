1f537131f2a058fb3aeacd67bd4af1281d122f1b
chonchon0516 <azumi.toyoda@gmail.com>
2022-12-08 13:02:30 +0900

    HTML, CSS[基礎編上級] 課題1: 構成を考えよう

---
+++ src/public/q1.php
@@ -0,0 +1,11 @@
+<div class="content">
+	<div class="pageTitleWrapper">
+		<h1 class="pageTitle">なぜTECH QUESTなのか</h1>
+	</div>
+	<div class="textWrapper">
+		<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
+	</div>
+  <div class="imgWrapper">
+		<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
+	</div>
+</div>
