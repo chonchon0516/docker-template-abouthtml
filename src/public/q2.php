d5bada24ef8aebaac8a3452a6a092201129564d7
chonchon0516 <azumi.toyoda@gmail.com>
2022-12-08 21:27:27 +0900

    HTML, CSS[基礎編上級] 課題2: 構成を考えよう

---
+++ src/public/q2.php
@@ -0,0 +1,14 @@
+<div class="content">
+	<div class="imgWrapper">
+		<img src="./img/christina-wocintechchat-com-68c8TlX5dfk-unsplash.jpg" class="img">
+	</div>
+
+  <div class="pageTitleWrapper">
+		<h1 class="pageTitle">TECH QUEST メンターの想い</h1>
+	</div>
+
+	<div class="textWrapper">
+		<p class="text">楽をするのではなく、時間を掛けてでも「正しいスキル」を習得する必要があると理解されている皆さまにこそ、より実践的な技術を身につけて欲しいと願っています。
+    是非、高い技術力を持ったエンジニアになって、素晴らしい人生への歩みを進めていきましょう！！</p>
+	</div>
+</div>
