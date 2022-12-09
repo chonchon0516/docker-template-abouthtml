3ab86ffde8bc00d73a8ddcb5a0faa3dc0f67f237
chonchon0516 <azumi.toyoda@gmail.com>
2022-12-09 21:34:02 +0900

    pタグ追加

--- src/public/q1.php
+++ src/public/q1.php
@@ -3,7 +3,8 @@
 		<h1 class="pageTitle">なぜTECH QUESTなのか</h1>
 	</div>
 	<div class="textWrapper">
-		<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
+		<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。</p>
+		<p class="text">QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
 	</div>
   <div class="imgWrapper">
 		<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
