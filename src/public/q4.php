a357a0155b9e6435aea607cc278f2b7a8fa16e0a
chonchon0516 <azumi.toyoda@gmail.com>
2022-12-19 22:01:16 +0900

    HTML, CSS[基礎編上級] 課題4: 構成を考えよう

---
+++ src/public/q4.php
@@ -0,0 +1,41 @@
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/q4.css">
+  <title>Document</title>
+</head>
+<body class="body">
+	<div class="left-content">
+		<div>
+			<div class="pageTitleWrapper">
+				<h1 class="pageTitle">なぜTECH QUESTなのか</h1>
+			</div>
+			<div class="textWrapper">
+				<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。</p>
+				<p class="text">TECH QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
+			</div>
+		</div>
+		<div class="imgWrapper">
+			<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
+		</div>
+		
+
+	</div>
+
+	<div class="right-content">
+
+		<div class="imgWrapper">
+			<img src="./img/christina-wocintechchat-com-68c8TlX5dfk-unsplash.jpg" class="img">
+		</div>
+   <div>
+		<div class="sectionTitleWrapper">
+			<h1 class="sectionTitle">TECH QUEST メンターの想い</h2>
+		</div>
+		<div class="textWrapper">
+			<p class="text">楽をするのではなく、時間を掛けてでも「正しいスキル」を習得する必要があると理解されている皆さまにこそ、より実践的な技術を身につけて欲しいと願っています。</p>
+			<p class="text">是非、高い技術力を持ったエンジニアになって、素晴らしい人生への歩みを進めていきましょう！！</p>
+		</div>
+	 </div>
+	</div>
+</body>
