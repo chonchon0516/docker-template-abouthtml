55737a9f2b0e5ecc41487f98c002a1317838edcf
chonchon0516 <azumi.toyoda@gmail.com>
2022-12-21 08:37:35 +0900

    コンテンツ修正

--- src/public/q4.php
+++ src/public/q4.php
@@ -6,11 +6,9 @@
   <title>Document</title>
 </head>
 <body class="body">
-	<div class="left-content">
-		<div>
-			<div class="pageTitleWrapper">
-				<h1 class="pageTitle">なぜTECH QUESTなのか</h1>
-			</div>
+	<div class="content">
+		<div class="contentDetail">
+			<h1 class="title">なぜTECH QUESTなのか</h1>	
 			<div class="textWrapper">
 				<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。</p>
 				<p class="text">TECH QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
@@ -19,23 +17,17 @@
 		<div class="imgWrapper">
 			<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
 		</div>
-		
-
 	</div>
-
-	<div class="right-content">
-
+	<div class="content">
 		<div class="imgWrapper">
 			<img src="./img/christina-wocintechchat-com-68c8TlX5dfk-unsplash.jpg" class="img">
 		</div>
-   <div>
-		<div class="sectionTitleWrapper">
-			<h1 class="sectionTitle">TECH QUEST メンターの想い</h2>
-		</div>
-		<div class="textWrapper">
-			<p class="text">楽をするのではなく、時間を掛けてでも「正しいスキル」を習得する必要があると理解されている皆さまにこそ、より実践的な技術を身につけて欲しいと願っています。</p>
-			<p class="text">是非、高い技術力を持ったエンジニアになって、素晴らしい人生への歩みを進めていきましょう！！</p>
-		</div>
-	 </div>
+   	<div class ="contentDetail">
+			<h1 class="title">TECH QUEST メンターの想い</h1>
+			<div class="textWrapper">
+				<p class="text">楽をするのではなく、時間を掛けてでも「正しいスキル」を習得する必要があると理解されている皆さまにこそ、より実践的な技術を身につけて欲しいと願っています。</p>
+				<p class="text">是非、高い技術力を持ったエンジニアになって、素晴らしい人生への歩みを進めていきましょう！！</p>
+			</div>
+	 	</div>
 	</div>
 </body>
