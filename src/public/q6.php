af8d974648051b709051291ed2f178ab4fb342cd
chonchon0516 <azumi.toyoda@gmail.com>
2023-01-06 21:45:53 +0900

    HTML, CSS[基礎編上級] 課題6: 構成を考えよう

---
+++ src/public/q6.php
@@ -0,0 +1,61 @@
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/q6.css">
+  <title>Document</title>
+</head>
+<body class="body">
+	<div class="main">
+		<h1 class="mainTitle">本当のあなたになろう</hi>
+		<h2 class="secondTitle">一発逆転のスキルを手に入れる！</h2>
+	</div>
+	<div class="content">
+    <div class="imgWrapper">
+			<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
+		</div>
+		<div class="contentDetail">
+			<h1 class="title">これからのエンジニア市場</h1>			
+			<div class="textWrapper">
+				<p class="text">2022年現在、非常に複雑で高機能なアプリが増えてきています。</p>
+				<p class="text">そのため「とりあえず動くコード」しか書けないエンジニアは遅かれ早かれ淘汰されていくと感じています。</p>
+        <p class="text">そんな時代を見据え、私たちTECH QUESTでは「キレイな設計、キレイなコード」を書けるエンジニアを輩出していきたいと考えています。</p>
+			</div>
+		</div>
+	</div>
+	<div class="content">
+   	<div class ="contentDetail">
+			<h1 class="title">なぜTECH QUESTなのか</h1>
+			<div class="textWrapper">
+				<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。</p>
+				<p class="text">TECH QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
+			</div>
+	 	</div>
+    <div class="imgWrapper">
+			<img src="./img/stephanie-krist-zW4rvExIVcQ-unsplash.jpg" class="img">
+		</div>
+	</div>
+  <div class="content">
+    <div class="imgWrapper">
+			<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
+		</div>
+		<div class="contentDetail">
+			<h1 class="title">TECH QUESTなのかTECH QUEST メンターの想い</h1>			
+			<div class="textWrapper">
+				<p class="text">楽をするのではなく、時間を掛けてでも「正しいスキル」を習得する必要があると理解されている皆さまにこそ、より実践的な技術を身につけて欲しいと願っています。</p>
+				<p class="text">是非、高い技術力を持ったエンジニアになって、素晴らしい人生への歩みを進めていきましょう！！</p>
+			</div>
+		</div>
+	</div>
+  
+	<div class="container">
+		<div class="box content-box">
+			<h2 class="text-tltle">これからの時代で必要なエンジニアリングスキル</h2>
+		</div>
+	</div>
+ <div class="main-footer">
+	 <h1 class = title-footer>2025年の崖とエンジニアのスキル不足<h1>
+
+ </div>
+	
+</body>
