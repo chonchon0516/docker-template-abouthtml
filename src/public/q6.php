3a5ca47b9ce3ee28d7a4bfbbc9afa11014bbf88e
KatsuyaTanaka95 <gu0520kt@gmail.com>
2023-01-07 22:40:15 +0900

    修正(田中メンター)

--- src/public/q6.php
+++ src/public/q6.php
@@ -5,11 +5,14 @@
   <link rel="stylesheet" href="../css/q6.css">
   <title>Document</title>
 </head>
+
 <body class="body">
+
 	<div class="main">
 		<h1 class="mainTitle">本当のあなたになろう</hi>
 		<h2 class="secondTitle">一発逆転のスキルを手に入れる！</h2>
 	</div>
+
 	<div class="content">
     <div class="imgWrapper">
 			<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
@@ -23,22 +26,31 @@
 			</div>
 		</div>
 	</div>
+
 	<div class="content">
-   	<div class ="contentDetail">
+		<div class ="contentDetail">
+
 			<h1 class="title">なぜTECH QUESTなのか</h1>
+
 			<div class="textWrapper">
 				<p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。</p>
 				<p class="text">TECH QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
 			</div>
-	 	</div>
+
+		</div>
+
     <div class="imgWrapper">
 			<img src="./img/stephanie-krist-zW4rvExIVcQ-unsplash.jpg" class="img">
 		</div>
+
 	</div>
+
   <div class="content">
+		
     <div class="imgWrapper">
 			<img src="./img/richard-brutyo-G0giT3AAWIo-unsplash.jpg" class="img">
 		</div>
+
 		<div class="contentDetail">
 			<h1 class="title">TECH QUESTなのかTECH QUEST メンターの想い</h1>			
 			<div class="textWrapper">
@@ -46,16 +58,17 @@
 				<p class="text">是非、高い技術力を持ったエンジニアになって、素晴らしい人生への歩みを進めていきましょう！！</p>
 			</div>
 		</div>
+
 	</div>
   
 	<div class="container">
-		<div class="box content-box">
+		<div class="box">
 			<h2 class="text-tltle">これからの時代で必要なエンジニアリングスキル</h2>
 		</div>
 	</div>
- <div class="main-footer">
-	 <h1 class = title-footer>2025年の崖とエンジニアのスキル不足<h1>
 
- </div>
+	<div class="main-footer">
+		<h1 class = title-footer>2025年の崖とエンジニアのスキル不足<h1>
+	</div>
 	
 </body>
