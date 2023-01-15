d40b44c9be8823b95ccd3a15aee4cf9e8b39f1f8
chonchon0516 <azumi.toyoda@gmail.com>
2023-01-15 09:35:27 +0900

    footer作成

--- src/public/q6.php
+++ src/public/q6.php
@@ -7,8 +7,9 @@
 </head>
 <body class="body">
 	<div class="main">
-		<h1 class="mainTitle">本当のあなたになろう</hi>
-		<h2 class="secondTitle">一発逆転のスキルを手に入れる！</h2>
+		<div class="boxMain">
+			<h1 class="main-title">本当のあなたになろう<span class="second-title">一発逆転のスキルを手に入れる！</span></h1>
+		</div>
 	</div>
 	<div class="content">
     <div class="imgWrapper">
@@ -50,12 +51,37 @@
   
 	<div class="container">
 		<div class="box content-box">
-			<h2 class="text-tltle">これからの時代で必要なエンジニアリングスキル</h2>
+			<h1 class="text-tltle">これからの時代で必要なエンジニアリングスキル</h1>
 		</div>
 	</div>
- <div class="main-footer">
-	 <h1 class = title-footer>2025年の崖とエンジニアのスキル不足<h1>
+	<div class = footer-title>
+ 		<h1 class = footerWrapper>2025年の崖とエンジニアのスキル不足<h1>
+	</div>
+	<div class="main-footer">
+		<p class = footer-content>皆さんは「2025年の崖」という言葉をご存知でしょうか？</p>
 
+		<p class = footer-content>これは経済産業省が発表している、「2025年を境に最大12兆円の経済損失が起こる」という課題です。</P>
+
+		<p class = footer-content>これは色々な原因がありますが、1つには<span class = "redColor">「日本のITエンジニアのスキル不足」</span>が挙げられます。</p>
+
+		<p class = footer-content>というのも、世の中のアプリはGoogleなどのGAFAをはじめ、どんどんハイレベル・ハイクオリティになっていっています。</p>
+
+		<p class = footer-content>日本がそういった企業・サービスと競争していくためには、よりハイクオリティなアプリを開発する必要があるので、<span class = "boldBlack">必然的にエンジニアにはそれだけの「技術力」が求められるようになっていくのです。</span></	<p>
+
+		<p class = footer-content>したがって、今後求められるエンジニアのレベルは今まで以上に高くなり、<span class = "boldBlack">「独学などで少し学んで現場に出るくらいでは2025年以降では通用しなくなる可能性が高い」</span>と、我々は考えています。</p>
+
+		<p class = footer-content>そのため、早い段階で「実践的かつ高度なコーディング・設計スキル」を身につけておくことが、今後のエンジニア人生をより良いものにしていくことになると確信しています。</p>
+
+		<p class = footer-content><span class = "boldBlack">「オブジェクト指向プログラミング」や「ドメイン駆動設計」「テスト駆動開発」といったことを、当然のように説明できて、実装・設計できるくらいでなければ生き残っていくことは難しいでしょう。</span></p>
  </div>
-	
+<div class = "bottomFooter">
+	<div class ="footercontent">
+		<h2 class = "footerTitle">ご質問・ご相談などありましたら<span class = "secondtitle">お気軽にお問い合わせください</span></h2>
+		<div class = "application">
+			<a href="#" class="appliTitle">無料体験 申し込み</a> 
+		</div>
+	</div>
+</div>
+
+
 </body>
