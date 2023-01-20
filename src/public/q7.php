a27d9a5c9bb20450b1df40fe23cbcd35941c03cf
chonchon0516 <azumi.toyoda@gmail.com>
2023-01-20 20:37:39 +0900

    HTML, CSS[基礎編上級] 課題7: 構成を考えよう

---
+++ src/public/q7.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/q7.css">
+  <title>Document</title>
+</head>
+<body class="body">
+	<div class="main">
+		<div class="boxMain">
+			<h2 class="main-title">メンター紹介</h2>
+		</div>
+	</div>
+	<div class="content">
+		<h2 class="contentWrapper">Mentor</h2>
+		<p class= "sentence">弊社メンターは全員、現役エンジニアです。</p>
+	</div>
+	<div class="introduceWrapper">
+			<div class="item">
+				<div class ="imageItem">
+					<img src="https://tech-quest.club/wp-content/uploads/2021/12/wp_menter-1-scaled.jpg" alt="ライオンの画像" width="200" height="229" border="0">
+				</div>
+				<h3 class="itemTitle">ウチダ アキヒロ<h3>
+				<p class="personalSentence">和服エンジニア。実務8年ほどのフルスタックです。趣味はギターとお酒と読書。おしゃべりが大好きです。喋りすぎていたら誰か止めてください。</p>
+			</div>
+			<div class="item">
+				<div class ="imageItem">
+					<img src="https://tech-quest.club/wp-content/uploads/2021/12/IMG_6866.jpg" alt="ライオンの画像" width="200" height="229" border="0">
+				</div>
+				<h3 class="itemTitle">田中 克弥<h3>
+				<p class="personalSentence">オンライン教室最高責任者。学習開始から数ヶ月でメンターに。かなりの実力派。バレーボール全国3位の実績もあるスーパーマンです。</p>
+			</div>
+			<div class="item">
+				<div class ="imageItem"><img src="https://tech-quest.club/wp-content/uploads/2021/12/image.png"alt="ライオンの画像" width="200" height="229" border="0"></div>
+				<h3 class="itemTitle">あおい<h3>
+				<p class="personalSentence">ユーモア溢れるムードメーカー。ほとんどのことは笑ってやり過ごす。ただ、説明のわかりやすさはピカイチです。無料カウンセリングの窓口担当でもある。</p>
+			</div>
+	</div>
+	<div class="boxFooter">
+		<h2 class="footer-title">代表の言葉</h2>
+	</div>
+	<div mainFooter>
+		<h3 class="footerTitle">「TOKYO MX」への出演</h3>
+		<p class="footerWrapper">HISTORYに出演させていただきました！</p>
+			<p class="footerWrapper">対談の様子は下記よりご確認いただけます。</p>
+	</div>
+</body>
