2347f5294491e246fd0da9a8ff71d9efe5f8a6b8
chonchon0516 <azumi.toyoda@gmail.com>
2023-01-23 13:30:19 +0900

    「TOKYO MX」への出演center部分について

--- src/public/q7.php
+++ src/public/q7.php
@@ -21,20 +21,20 @@
 	<div class="introduceWrapper">
 			<div class="item">
 				<div class ="imageItem">
-					<img src="https://tech-quest.club/wp-content/uploads/2021/12/wp_menter-1-scaled.jpg" alt="ライオンの画像" width="200" height="229" border="0">
+					<img src="https://tech-quest.club/wp-content/uploads/2021/12/wp_menter-1-scaled.jpg" alt="ライオンの画像" width="200" height="229" border="0" class="img">
 				</div>
 				<h3 class="itemTitle">ウチダ アキヒロ<h3>
 				<p class="personalSentence">和服エンジニア。実務8年ほどのフルスタックです。趣味はギターとお酒と読書。おしゃべりが大好きです。喋りすぎていたら誰か止めてください。</p>
 			</div>
 			<div class="item">
 				<div class ="imageItem">
-					<img src="https://tech-quest.club/wp-content/uploads/2021/12/IMG_6866.jpg" alt="ライオンの画像" width="200" height="229" border="0">
+					<img src="https://tech-quest.club/wp-content/uploads/2021/12/IMG_6866.jpg" alt="ライオンの画像" width="200" height="229" border="0" class="img">
 				</div>
 				<h3 class="itemTitle">田中 克弥<h3>
 				<p class="personalSentence">オンライン教室最高責任者。学習開始から数ヶ月でメンターに。かなりの実力派。バレーボール全国3位の実績もあるスーパーマンです。</p>
 			</div>
 			<div class="item">
-				<div class ="imageItem"><img src="https://tech-quest.club/wp-content/uploads/2021/12/image.png"alt="ライオンの画像" width="200" height="229" border="0"></div>
+				<div class ="imageItem"><img src="https://tech-quest.club/wp-content/uploads/2021/12/image.png"alt="ライオンの画像" width="200" height="229" border="0" class="img"></div>
 				<h3 class="itemTitle">あおい<h3>
 				<p class="personalSentence">ユーモア溢れるムードメーカー。ほとんどのことは笑ってやり過ごす。ただ、説明のわかりやすさはピカイチです。無料カウンセリングの窓口担当でもある。</p>
 			</div>
@@ -46,5 +46,10 @@
 		<h3 class="footerTitle">「TOKYO MX」への出演</h3>
 		<p class="footerWrapper">HISTORYに出演させていただきました！</p>
 			<p class="footerWrapper">対談の様子は下記よりご確認いただけます。</p>
+			<div class = "footerLink">
+				<div class="tbsRadio">
+					<a title="HISTORY 御業合同会社 内田明宏" href="https://history-tv.jp/backnumber/uchida_akihiro/" target="_blank" rel="noopener"><img loading="lazy" src="https://history-tv.jp/img/banner/uchida_akihiro/h_banner_01.jpg" alt="HISTORY 御業合同会社 内田明宏" width="400" height="150" border="0"></a>
+				</div>
+			</div>
 	</div>
 </body>
